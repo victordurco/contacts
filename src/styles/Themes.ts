40472d21fb610847f7bc293a31a0455f0465d6e5
victordurco <victordurco@gmail.com>
2022-02-28 03:32:12 -0300

    created dark theme

---
+++ src/styles/Themes.ts
@@ -0,0 +1,16 @@
+const darkTheme = {
+  colors: {
+      header: "#3B72E5",
+      background: "#0A1929",
+      searchInput: "#31BE00",
+      contactBox: "#132F4C",
+      contactBoxBorder: "#5090D3",
+      infoBox: "#0A1E3C",
+      boxButton: "#428BE3",
+      deleteButton: "#940000",
+      addButton: "#31BE00",
+      text: "#fff"
+   }
+}
+
+export default darkTheme;
