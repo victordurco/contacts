77393dba8a3d157d45c1131ccba0cac034896416
victordurco <victordurco@gmail.com>
2022-03-02 15:30:12 -0300

    create contact integration test done

---
+++ cypress/integration/createContact.spec.ts
@@ -0,0 +1,23 @@
+/* eslint-disable no-undef */
+/// <reference types="cypress" />
+afterEach(() => {
+  cy.request({
+    method: 'DELETE',
+    url: 'http://localhost:4000/contacts',
+    body: {
+      email: 'emailteste@gmail.com',
+    },
+    failOnStatusCode: false,
+  });
+});
+
+describe('Create', () => {
+  it('should create contact successfully', () => {
+    cy.visit('http://localhost:3000/contato');
+    cy.get('input[type=text]').type('Nome Teste');
+    cy.get('input[type=tel]').type('(55) 55555-5555');
+    cy.get('input[type=email]').type('emailteste@gmail.com');
+    cy.contains('Salvar').click();
+    cy.url().should('equal', 'http://localhost:3000/');
+  });
+});
